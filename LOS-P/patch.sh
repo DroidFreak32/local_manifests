2a6bf02e2ee5190ca7c6ad2857aa4c824f59e992
DroidFreak32 <rushabshah32@gmail.com>
2019-06-15 02:05:19 +0000

    Update LineageOS 16 for surnia

--- LOS-P/patch.sh
+++ LOS-P/patch.sh
@@ -1,39 +1,28 @@
+#!/bin/sh
 
 TOP=${PWD}
+echo "TOP: $TOP"
+
 . build/envsetup.sh
 
 #############################################################
 # PATCHES                                                   #
 #############################################################
 
+##
+##  Lag Fixes
+##
+cd frameworks/base
+git am -3 $TOP/0001-Improve-performance-of-unclipped-save-layers.patch
+cd $TOP
+
+cd external/skia
+git am -3 $TOP/0001-Add-rect-parameter-to-makeImageSnapshot-and-stop-usi.patch
+git am -3 $TOP/0002-fGpu-is-null-when-GrGpuResource-release.patch
+cd $TOP
 
 #############################################################
 # CHERRYPICKS                                               #
 #############################################################
 
 # Example: repopick [CHANGE_NUMBER]
-
-# frameworks/base
-repopick 226343 229230 229612
-
-# frameworks/native
-repopick 224530 225542 225543 225546
-
-# android_hardware_interfaces
-repopick 225506 225507
-
-# android_hardware_qcom_audio
-repopick 223450
-
-# android_hardware_qcom_display
-repopick -t 8916-display-p -e 223444
-
-# android_hardware_qcom_wlan
-#repopick 226638 226643
-
-# Sepolicy
-repopick 230613
-repopick -t pie-qcom-legacy-sepolicy
-repopick -t pie-qcom-sepolicy
-repopick 230229-230233 230237 234832 237211 235455
-repopick 235258
