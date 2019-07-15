59ebbb50c1d2ae94451e76ec6d513a34638c9f85
DroidFreak32 <rushabshah32@gmail.com>
2019-07-15 11:31:13 +0530

    Update LOS-P patches

--- LOS-P/patch.sh
+++ LOS-P/patch.sh
@@ -5,6 +5,16 @@ echo "TOP: $TOP"
 
 . build/envsetup.sh
 
+# safer repopick:
+# checks for the return value of repopick and waits for
+# user input to continue.
+
+safer-repopick() {
+    repopick $*
+    if [ "$?" -gt "0" ] ; then
+      read -p "$0 $*: ENTER to continue"
+    fi
+}
 #############################################################
 # PATCHES                                                   #
 #############################################################
@@ -12,12 +22,8 @@ echo "TOP: $TOP"
 ##
 ##  Lag Fixes
 ##
-cd frameworks/base
-git am -3 $TOP/0001-Improve-performance-of-unclipped-save-layers.patch
-cd $TOP
-
+safer-repopick -t unclipped_layer
 cd external/skia
-git am -3 $TOP/0001-Add-rect-parameter-to-makeImageSnapshot-and-stop-usi.patch
 git am -3 $TOP/0002-fGpu-is-null-when-GrGpuResource-release.patch
 cd $TOP
 
@@ -26,3 +32,36 @@ cd $TOP
 #############################################################
 
 # Example: repopick [CHANGE_NUMBER]
+# Sounds: Squashed cleanup of sound files
+safer-repopick 236765 -P frameworks/base -f
+
+# SDCLANG-6: fix declaration
+safer-repopick 219760 -P system/nfc/
+
+# pie-msim-ringtones
+safer-repopick -t pie-msim-ringtones -e 233633,233634,237209
+safer-repopick 233633 -P frameworks/base/
+safer-repopick 233634 -P packages/apps/Settings/
+safer-repopick 237209 -P vendor/lineage/
+
+# RIP libhealthd.lineage
+safer-repopick 242432 -P vendor/lineage/
+
+# Make custom off-mode charging screen great again
+safer-repopick 242433 -P vendor/lineage/
+
+# p-display-shrink
+safer-repopick 231827 -P frameworks/base/
+safer-repopick 231847 -P frameworks/base/
+safer-repopick 231848 -P frameworks/base/
+safer-repopick 231851 -P frameworks/base/
+safer-repopick 231852 -P frameworks/base/
+safer-repopick 237500 -P packages/apps/Settings
+safer-repopick 231828
+
+# swipe_screenshot
+safer-repopick 247884 -P packages/apps/Settings
+safer-repopick 247885 -P lineage-sdk/
+safer-repopick 247886 -P frameworks/base/
+
+safer-repopick -t pie-onehandmode-tile
